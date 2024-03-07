49c5b7b05e86e339d14c9f960db9322b9f6de702
creamlaflare <antonkazachenko2002@gmail.com>
2024-03-07 12:18:16 -0800

    Implement the conversion

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,10 +1,16 @@
 package encoding
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/Yandex-Practicum/final-project-encoding-go/models"
+	"gopkg.in/yaml.v3"
+	"io"
+	"os"
 )
 
 // JSONData тип для перекодирования из JSON в YAML
+// JSONData type for converting from JSON to YAML
 type JSONData struct {
 	DockerCompose *models.DockerCompose
 	FileInput     string
@@ -12,6 +18,7 @@ type JSONData struct {
 }
 
 // YAMLData тип для перекодирования из YAML в JSON
+// YAMLData type for converting from YAML to JSON
 type YAMLData struct {
 	DockerCompose *models.DockerCompose
 	FileInput     string
@@ -19,22 +26,69 @@ type YAMLData struct {
 }
 
 // MyEncoder интерфейс для структур YAMLData и JSONData
+// MyEncoder interface for YAMLData and JSONData structures
 type MyEncoder interface {
 	Encoding() error
 }
 
 // Encoding перекодирует файл из JSON в YAML
+// Encoding converts a file from JSON to YAML
 func (j *JSONData) Encoding() error {
-	// ниже реализуйте метод
-	// ...
+	jsonFile, err := os.Open(j.FileInput)
+	defer jsonFile.Close()
+	if err != nil {
+		return err
+	}
+	buf, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(buf, &j.DockerCompose); err != nil {
+		return err
+	}
+	yamlData, err := yaml.Marshal(&j.DockerCompose)
+	f, err := os.Create(j.FileOutput)
+	if err != nil {
+		fmt.Printf("ошибка при создании файла: %s", err.Error())
+		return err
+	}
+	defer f.Close()
 
+	_, err = f.Write(yamlData)
+	if err != nil {
+		fmt.Printf("ошибка при записи данных в файл: %s", err.Error())
+		return err
+	}
 	return nil
 }
 
 // Encoding перекодирует файл из YAML в JSON
+// Encoding converts a file from YAML to JSON
 func (y *YAMLData) Encoding() error {
-	// Ниже реализуйте метод
-	// ...
+	yamlFile, err := os.Open(y.FileInput)
+	defer yamlFile.Close()
+	if err != nil {
+		return err
+	}
+	buf, err := io.ReadAll(yamlFile)
+	if err != nil {
+		return err
+	}
+	if err := yaml.Unmarshal(buf, &y.DockerCompose); err != nil {
+		return err
+	}
+	yamlData, err := json.Marshal(&y.DockerCompose)
+	f, err := os.Create(y.FileOutput)
+	if err != nil {
+		fmt.Printf("ошибка при создании файла: %s", err.Error())
+		return err
+	}
+	defer f.Close()
 
+	_, err = f.Write(yamlData)
+	if err != nil {
+		fmt.Printf("ошибка при записи данных в файл: %s", err.Error())
+		return err
+	}
 	return nil
 }
